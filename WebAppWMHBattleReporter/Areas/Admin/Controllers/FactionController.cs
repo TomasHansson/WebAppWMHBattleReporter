76726b4b60bb61d7f17e7becaf4888b1bfe59bd7
Tomas Hansson <45672109+TomasHansson@users.noreply.github.com>
2019-05-30 15:04:43 +0200

    Implement full CRUD-capabilities for Factions.

--- WebAppWMHBattleReporter/Areas/Admin/Controllers/FactionController.cs
+++ WebAppWMHBattleReporter/Areas/Admin/Controllers/FactionController.cs
@@ -35,7 +35,8 @@ namespace WebAppWMHBattleReporter.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(FactionViewModel viewModel)
         {
-            if(!ModelState.IsValid)
+            viewModel.StatusMessage = string.Empty;
+            if (!ModelState.IsValid)
                 return View(viewModel);
 
             if(await _db.Factions.AnyAsync(f => f.Name == viewModel.Faction.Name))
@@ -45,6 +46,85 @@ namespace WebAppWMHBattleReporter.Areas.Admin.Controllers
             }
 
             _db.Factions.Add(viewModel.Faction);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Faction factionFromDB = await _db.Factions.FindAsync(id);
+            if (factionFromDB == null)
+                return NotFound();
+
+            return View(factionFromDB);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Faction factionFromDB = await _db.Factions.FindAsync(id);
+            if (factionFromDB == null)
+                return NotFound();
+
+            FactionViewModel viewModel = new FactionViewModel()
+            {
+                Faction = factionFromDB
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(FactionViewModel viewModel)
+        {
+            viewModel.StatusMessage = string.Empty;
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            if (await _db.Factions.AnyAsync(f => f.Name == viewModel.Faction.Name))
+            {
+                viewModel.StatusMessage = "Error: A faction with that name already exists in the database.";
+                return View(viewModel);
+            }
+
+            Faction factionFromDB = await _db.Factions.FindAsync(viewModel.Faction.Id);
+            if (factionFromDB == null)
+                return NotFound();
+
+            factionFromDB.Name = viewModel.Faction.Name;
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Faction factionFromDB = await _db.Factions.FindAsync(id);
+            if (factionFromDB == null)
+                return NotFound();
+
+            return View(factionFromDB);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Faction factionFromDB = await _db.Factions.FindAsync(id);
+            if (factionFromDB == null)
+                return NotFound();
+
+            _db.Factions.Remove(factionFromDB);
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
