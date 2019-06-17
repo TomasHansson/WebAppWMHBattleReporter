5a3b0465fdc316d2a0f8574f10b426401ccf5a71
Tomas Hansson <tomashansson1984@gmail.com>
2019-06-17 22:31:04 +0200

    Add ability for list of themes and casters for both poster and opponent to update upon selecting a new faction for either of the two players.

--- WebAppWMHBattleReporter/Areas/EndUser/Controllers/BattleReportController.cs
+++ WebAppWMHBattleReporter/Areas/EndUser/Controllers/BattleReportController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebAppWMHBattleReporter.Data;
 using WebAppWMHBattleReporter.Models;
@@ -33,6 +34,30 @@ namespace WebAppWMHBattleReporter.Areas.EndUser.Controllers
             viewModel.FirstFactionCasters = await _db.Casters.Where(c => c.FactionId == firstFactionId).ToListAsync();
             viewModel.BattleReport.PostersUsername = "Posters Username"; // This should be changed to fetch the username with the use of the UserClaim.
             return View(viewModel);
+        }
+
+        public async Task<IActionResult> GetFactionThemeNames(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            if (!await _db.Factions.AnyAsync(f => f.Id == id))
+                return NotFound();
+
+            List<string> factionThemes = await _db.Themes.Where(t => t.FactionId == id).Select(t => t.Name).ToListAsync();
+            return Json(factionThemes);
+        }
+
+        public async Task<IActionResult> GetFactionCasterNames(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            if (!await _db.Factions.AnyAsync(f => f.Id == id))
+                return NotFound();
+
+            List<string> factionCasters = await _db.Casters.Where(c => c.FactionId == id).Select(f => f.Name).ToListAsync();
+            return Json(factionCasters);
         }
     }
 }
