6b2d85cbafe9ebd499b2e4fa907aa95a3fa0e73b
Tomas Hansson <tomashansson1984@gmail.com>
2019-06-30 18:49:42 +0200

    Add StatisticsController and the Index-view which will show a DashBoard of the Top 10 Factions, Themes and Casters.

---
+++ WebAppWMHBattleReporter/Areas/EndUser/Controllers/StatisticsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAppWMHBattleReporter.Data;
+using WebAppWMHBattleReporter.Models.ViewModels;
+
+namespace WebAppWMHBattleReporter.Areas.EndUser.Controllers
+{
+    [Area("EndUser")]
+    public class StatisticsController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public StatisticsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            DashBoardViewModel viewModel = new DashBoardViewModel()
+            {
+                Top10Factions = await _db.Factions.OrderByDescending(f => f.Winrate).Take(10).ToListAsync(),
+                Top10Themes = await _db.Themes.OrderByDescending(t => t.Winrate).Take(10).ToListAsync(),
+                Top10Casters = await _db.Casters.OrderByDescending(c => c.Winrate).Take(10).ToListAsync()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ WebAppWMHBattleReporter/Models/ViewModels/DashBoardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppWMHBattleReporter.Models.ViewModels
+{
+    public class DashBoardViewModel
+    {
+        public List<Faction> Top10Factions { get; set; }
+        public List<Theme> Top10Themes { get; set; }
+        public List<Caster> Top10Casters { get; set; }
+    }
+}
