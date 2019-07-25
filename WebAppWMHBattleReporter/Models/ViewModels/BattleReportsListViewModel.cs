70cbf04899d4acffed1af47a381217925de92123
Tomas Hansson <tomashansson1984@gmail.com>
2019-07-25 20:06:08 +0200

    Change design och the differenet Details-oriented shared views, to be similar in style. The new BattleReportDetails shared view is now used in all cases where such information is to be presented.

---
+++ WebAppWMHBattleReporter/Models/ViewModels/BattleReportsListViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppWMHBattleReporter.Models.ViewModels
+{
+    public class BattleReportsListViewModel
+    {
+        public List<BattleReport> BattleReports { get; set; }
+        public List<string> TimePeriodOptions { get; set; }
+        public string TimePeriod { get; set; }
+        public List<string> FactionOptions { get; set; }
+        public string P1Faction { get; set; }
+        public List<string> P1ThemeOptions { get; set; }
+        public string P1Theme { get; set; }
+        public List<string> P1CasterOptions { get; set; }
+        public string P1Caster { get; set; }
+        public string P2Faction { get; set; }
+        public List<string> P2ThemeOptions { get; set; }
+        public string P2Theme { get; set; }
+        public List<string> P2CasterOptions { get; set; }
+        public string P2Caster { get; set; }
+        public List<string> GameSizeOptions { get; set; }
+        public string GameSize { get; set; }
+        public List<string> EndConditionOptions { get; set; }
+        public string EndCondition { get; set; }
+        public List<string> ScenarioOptions { get; set; }
+        public string Scenario { get; set; }
+        public bool HideFilters { get; set; }
+    }
+}
